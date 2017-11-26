6bd4fcf2f9b24d796ba199e117ed94b09b42b8f2
Ashutosh <remote add origin https://ashutoshmca13@bitbucket.org/ashutoshmca13/androidproject.git>
2017-11-27 00:45:32 +0530

    adding the base login screen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,20 @@
 package sample;
 
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+
 public class Controller {
 
+    public JFXTextField username;
+
+
+    public JFXTextField password;
+
+    public void login(ActionEvent actionEvent) {
+        System.out.println(username.getText());
+        System.out.println(password.getText());
+        System.out.println("helo");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,8 +11,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
